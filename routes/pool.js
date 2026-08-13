3bb9aac7cd78eebf4b117c7957bd6380213bb4df
RAJ SHAKYA <shakyaraj428@gmail.com>
2026-08-13 13:37:48 +0530

    Enable multiple statements in database connection

--- routes/pool.js
+++ routes/pool.js
@@ -5,7 +5,8 @@ var pool = mysql.createPool({
     port:3306,
     user:'root',
     password:'1234',
-    database:'batch56pm'
+    database:'batch56pm',
+    multipleStatements: true
 })
 
 module.exports = pool;
